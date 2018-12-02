dc829a3200209872d8e4929fc9a7d94fef6e4cd1
stephanie-m-gilley <42945120+stephanie-m-gilley@users.noreply.github.com>
2018-12-02 11:56:57 -0600

    Add files via upload
    completed project

---
+++ SearchController.java
@@ -0,0 +1,47 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.JobData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+@RequestMapping("search")
+public class SearchController {
+
+
+    @RequestMapping(value = "")
+    public String search(Model model) {
+        model.addAttribute("columns", ListController.columnChoices);
+        return "search";
+    }
+
+    // TODO #1 - Create handler to process search request and display results
+
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchType, String searchTerm) {
+
+        ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        ArrayList<HashMap<String, String>> allJobs = JobData.findByValue(searchTerm);
+
+        if (searchType.equals("all")) {
+            model.addAttribute("jobs", allJobs);
+        } else {
+            model.addAttribute("jobs", jobs);
+        }
+
+        model.addAttribute("columns", ListController.columnChoices);
+
+        return "search";
+
+    }
+
+}
